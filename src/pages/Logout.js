d97e286007e860c14d42e8ec21978d555b5af14b
samitkumarpatel <samitkumarpatel@gmail.com>
2023-08-31 20:52:21 +0000

    remove unused import

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -4,8 +4,6 @@ import { useNavigate } from 'react-router-dom';
 import { logout } from '../store/authSlice';
 
 const Logout = () => {
-    const [count, setCount] = useState(0);
-    const [countInTimeout, setCountInTimeout] = useState(0);
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
